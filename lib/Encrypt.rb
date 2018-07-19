2188b1264f7d8fbc293623e8cb7b913e128aaaad
Jack Branch <jackbranch@Jacks-MacBook-Pro.local>
2018-07-19 12:19:32 +0100

    encryption finished

--- lib/Encrypt.rb
+++ lib/Encrypt.rb
@@ -1,26 +1,38 @@
 # encryption class
 class Encrypt
   attr_reader :key # temporary attr reader - ideally should not be accessable
+
   def initialize
-    @key = gen_key
+    @letters = {}
+    gen_keys
   end
 
-  def gen_key
-    rand(1000..100_000_0) # generate a random key
+  private
+
+  def gen_keys
+    @key = rand(100..500) # generate a random key
+    @keys = (32..126).to_a.shuffle # all numbers relevent shuffled
+  end
+
+  def scramble(keys = @keys)
+    count = 0
+    (32..126).each do |i|
+      @letters[i] = keys[count].chr
+      count += 1
+    end
+    @letters # for rspec
   end
 
   def cypher(text)
-    output = ''
-    text.each_byte do |c|
-      output += c < 113 ? (((c + 3) * 1.5 - (c * 0.36)).to_i).chr : (c - 80).chr
-    end
-    return output
+    text.split('').map { |c| scramble.key(c).chr }.join('')
   end
+
+  public
 
   def encrypt(text, key)
     key.times do
       text = cypher(text)
     end
-    text
   end
+
 end
--- spec/Encrypt_spec.rb
+++ spec/Encrypt_spec.rb
@@ -1,23 +1,41 @@
 require './lib/Encrypt'
+require_relative 'test_variables'
 describe Encrypt do
+  let(:test_vars) { TestVariableHolder.new}
   let(:input_text) { ['a', 'abcde', 'jack', 'hello world'] }
-  let(:output_text) { ['s', 'stuvw', ';'] }
+  let(:key) { [1000, 9999, 5500] }
   context 'Feature_1: Encrypt' do
     describe '#encrypt' do
       it { expect(subject).to respond_to(:encrypt).with(2).arguments }
+      it 'should encrypt a message' do
+        expect(subject.encrypt('hello', 1)).not_to eq('hello') # has changed
+      end
     end
     describe '#cypher' do
-      it { expect(subject).to respond_to(:cypher).with(1).arguments }
-      it { expect(subject.cypher(input_text[0])).to eq(output_text[0]) }
-      it { expect(subject.cypher(input_text[1])).to eq(output_text[1]) }
+      it { expect(subject).respond_to?(:cypher, true) }
     end
-    describe '#key' do
-      it { expect(subject).to respond_to(:key) } # encryption key
-      it { expect(subject.key).to be_an(Integer) } # should be an int
-      describe '#gen_key' do
-        it { expect(subject).to respond_to(:gen_key) } # generate key
+    describe '#gen_keys' do
+      it { expect(subject).respond_to?(:gen_keys, true) } # generate key
+      it { expect(subject.send(:gen_keys)).to be_an(Array) } # should return correctly
+    end
+    describe '#scramble' do
+      it { expect(subject).respond_to?(:scramble, true) } # scramble alphabet
+      it { expect(subject.send(:scramble)).to be_a(Hash) } # should be a hash
+      it { expect(subject.send(:scramble).length).to eq(95) } # should be right length
+      describe 'scramble tests' do
+        it 'can scramble input letters correctly' do
+          expect(subject.send(:scramble, TestVariableHolder::TEST_KEY)).to eq(TestVariableHolder::TEST_HASH)
+        end
       end
     end
   end
+  context 'Feature_1 :Decrypt' do
+
+  end
+  context 'runt - time tests' do
+
+  end
 end
+
+
 
